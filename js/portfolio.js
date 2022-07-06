d460b1dd66487f6f734e39a517ee2413693c2a33
daeun1205 <102287829+daeun1205@users.noreply.github.com>
2022-07-06 16:14:22 +0900

    Update portfolio.js

--- js/portfolio.js
+++ js/portfolio.js
@@ -30,4 +30,43 @@ $(document).ready(function(){
 
     });
 
+    $('.menu_box li').eq(0).click(function(){
+        const sct = $(window).scrollTop();
+        const about = $('.section1').offset().top;
+        $('html,body').animate({
+            scrollTop: about-100
+        });
+
+        $('.shape').addClass('active');
+        $('.java_icon').addClass('active');
+        $('.js_icon').addClass('active');
+        $('.ps_icon,.figma_icon').addClass('active');
+        $('.ai_icon').addClass('active');
+        $('.xd_icon').addClass('active');
+    });
+
+    $('.menu_box li').eq(1).click(function(){
+        const sct = $(window).scrollTop();
+        const renewal = $('.section2').offset().top;
+        $('html,body').animate({
+            scrollTop: renewal-100
+        });
+    });
+
+    $('.menu_box li').eq(2).click(function(){
+        const sct = $(window).scrollTop();
+        const uiux = $('.section3').offset().top;
+        $('html,body').animate({
+            scrollTop: uiux-100
+        });
+    });
+
+    $('.menu_box li').eq(3).click(function(){
+        const sct = $(window).scrollTop();
+        const contact = $('.section4').offset().top;
+        $('html,body').animate({
+            scrollTop: contact-100
+        });
+    });
+        
 });//end
